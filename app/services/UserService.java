9b2933eaf7a56386b9ab033a3e79133f5364a85b
Markus Jura <markus.jura@gmail.com>
2015-06-03 17:06:13 +0200

    LogService

--- app/services/UserService.java
+++ app/services/UserService.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import models.LogMessage;
-import play.Logger;
 import play.libs.Json;
 import play.mvc.WebSocket;
 
